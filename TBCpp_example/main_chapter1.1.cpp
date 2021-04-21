f71a90687c012fd819a36083afea7924840e0904
wnsgud606 <wnsgud606@naver.com>
2021-04-21 22:37:37 +0900

    commit 1.2

--- TBCpp_example/main_chapter1.1.cpp
+++ TBCpp_example/main_chapter1.1.cpp
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int x=2; // literal
+    int x=9; // literal
     int y=x+3; // variable
 
     std::cout << y << std::endl;
---
+++ TBCpp_example/main_chapter1.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream> //preprocessor directive
+
+int main(void)
+{
+    int x=9; // literal
+    int y=x+3; // variable
+    
+    //1. ----
+    //2. ----
+
+    std::cout << y << std::endl; // << = output operator
+
+    int sight = 10; // set sight to be 0
+
+    sight = 0;
+
+    // 1+1 event
+    int cost = num_item / 2*price_per_item; // this code don't work. Just example of comment.
+
+
+    return 0;
+}
