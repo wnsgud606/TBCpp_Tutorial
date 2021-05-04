c1b25f9d5e76a4da79af6ad702c7c334ebc78fd2
wnsgud606 <wnsgud606@naver.com>
2021-05-04 23:18:35 +0900

    commit 21_05_04

---
+++ TBCpp_example/main_chapter1.6.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //preprocessor directive
+#include <cstdio> //printf
+
+using namespace std;
+
+void GetNumber()
+{
+// Function name usually consist of a combination of verbs and nouns because that describe the skill of a function
+// FUnction name usually use capital letter.
+}
+                   
+
+int main(void)
+{
+    int n_apples; // Variable name usually use small letter
+
+    
+
+    return 0;
+}
