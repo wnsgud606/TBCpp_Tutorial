694168f9a93d01216c146b6610760ea5a3b275ad
wnsgud606 <wnsgud606@naver.com>
2021-05-09 19:55:06 +0900

    commit 21_05_09

---
+++ TBCpp_example/main_chapter1.9.cpp
@@ -0,0 +1,29 @@
+//[1.9] Formatting
+
+#include <iostream> //preprocessor directive
+#include <cstdio> //printf
+
+using namespace std;
+
+int add(int x, int y)
+{
+    return x + y; // indenting is making, embty area
+}   
+
+int main(void)
+{
+    cout << "Hello,      World" << "asdfasdf" << 
+        "Hello Home" << endl;
+
+    int my_v            = 1;
+    int x               = 4;
+    int num_apples      = 123;
+
+    // this is important
+    int x = 1 + 2; 
+    
+    // this is important
+    int y = 3 + 4; 
+
+    return 0;
+}
